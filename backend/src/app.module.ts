013f2b9729968a0dd060f6be66b3502350e853fb
LouiMinister <louiminister@gmail.com>
2023-05-17 15:16:42 +0900

    chore: mongo connection setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URL)],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
