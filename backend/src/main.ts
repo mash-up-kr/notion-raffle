393faaea3d5f17fab95683d2fd4f4fbc8b0d5148
marades <jkl9510357@gmail.com>
2023-05-18 18:26:20 +0900

    Chore: cors 허용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+        cors: true,
+    });
 
     await app.listen(3000);
 }
