9fda544c717002dab70a43204e6b2919889916c8
marades <jkl9510357@gmail.com>
2023-05-19 16:59:21 +0900

    Feat: backend - landing api에 프론트 배포링크 반영

--- backend/src/landing/landing.controller.ts
+++ backend/src/landing/landing.controller.ts
@@ -1,24 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { getUUId } from '../util/uuid';
 
-@Controller()
+@Controller('api')
 export class LandingController {
-    // constructor() {}
-
-    @Get('landing-page')
-    routeToLandingPage(): string {
-        // TODO: 리액트 랜딩 페이지 반환하도록
-        return 'some react build file';
-    }
-
     @Get('uuid')
-    getUid(): string {
+    getUUid(): string {
         return getUUId();
     }
 
     @Get('embed-url')
     getEmbedUrl(): string {
-        // TODO: 임베딩 URL + uid로 반환하도록 구현
-        return `https://notion-raffle-embeded/${getUUId()}`;
+        return `https://notion-raffle-fe.vercel.app/embed/${getUUId()}`;
     }
 }
