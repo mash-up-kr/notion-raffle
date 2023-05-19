d9eaad2a11a98096748bb7197b69ec6eece317d7
LouiMinister <louiminister@gmail.com>
2023-05-19 15:55:47 +0900

    fix: 백엔드에서 프론트엔드 바라보지 않도록 연결 해제

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 import { LandingModule } from './landing/landing.module';
 import { DrawlotModule } from './drawlot/drawlot.module';
 
@@ -12,9 +12,9 @@ import { DrawlotModule } from './drawlot/drawlot.module';
     imports: [
         ConfigModule.forRoot(),
         MongooseModule.forRoot(process.env.MONGO_URL),
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '..', '..', 'frontend', 'build'),
-        }),
+        // ServeStaticModule.forRoot({
+        //     rootPath: join(__dirname, '..', '..', 'frontend', 'build'),
+        // }),
         LandingModule,
         DrawlotModule,
     ],
