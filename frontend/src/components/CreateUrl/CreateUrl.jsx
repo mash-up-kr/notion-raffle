505e17eb59e3863efa1366968b2fb6dd997f7cc4
LouiMinister <louiminister@gmail.com>
2023-05-19 12:51:35 +0900

    fix: url copy 페이지 css 수정

--- frontend/src/components/CreateUrl/CreateUrl.jsx
+++ frontend/src/components/CreateUrl/CreateUrl.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  ArrowPathIcon,
+  ClipboardDocumentListIcon,
+} from "@heroicons/react/24/outline";
 
 function CreateUrl() {
   const [url, setUrl] = useState("");
 
-  const handleClick = (e) => {
+  const handleClick = () => {
     fetch("http://localhost:3000/uuid") // 엔드포인트 URL을 입력하세요
       .then((response) => response.text())
       .then((data) => {
@@ -14,10 +18,13 @@ function CreateUrl() {
       });
   };
 
-  const handleCopy = (e) => {
-    const text = e.target.innerText;
+  useEffect(() => {
+    handleClick();
+  }, []);
+
+  const handleCopy = () => {
     navigator.clipboard
-      .writeText(text)
+      .writeText(url)
       .then(() => {
         alert("Text copied to clipboard");
       })
@@ -26,7 +33,6 @@ function CreateUrl() {
       });
   };
 
-
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -34,35 +40,29 @@ function CreateUrl() {
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">노션 임베드</h1>
             <p className="py-6">
-              아래 버튼을 눌러 임베드 URL을 생성해 보세요!! <br/><br/>
+              아래 버튼을 눌러 임베드 URL을 생성해 보세요!! <br />
+              <br />
               복사하려면 URL을 클릭해주세요!!
             </p>
             <div
-                  className="input input-bordered"
-                  style={{ height: "fit-content", minHeight: "3rem" }}
-                  onClick={handleCopy}
-                >
-                  {url !== "" ? url : "버튼을 눌러주세요"}
-                </div>
-            <div className="max-w-md">
-              <label
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  alignItems: "stretch",
-                  justifyContent: "center"
-                }}
+              className="input input-bordered flex items-center"
+              style={{ height: "fit-content", minHeight: "3rem" }}
+            >
+              <span className="flex-grow">
+                {url !== "" ? url : "버튼을 눌러주세요"}
+              </span>
+              <button
+                className="btn btn-square rounded-2xl"
+                onClick={() => handleClick()}
               >
-                
-
-                <button
-                  className="btn btn-primary"
-                  style={{ height: "inherit", minHeight: "3rem" }}
-                  onClick={() => handleClick()}
-                >
-                  Get Started
-                </button>
-              </label>
+                <ArrowPathIcon className="h-6 w-6 text-gray-1000" />
+              </button>
+              <button
+                className="btn btn-square rounded-2xl"
+                onClick={handleCopy}
+              >
+                <ClipboardDocumentListIcon className="h-6 w-6 text-gray-1000" />
+              </button>
             </div>
           </div>
         </div>
